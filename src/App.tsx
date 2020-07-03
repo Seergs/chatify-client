f642bb6b5f49f3dbe780c075c434c1fdb6f930ea
Sergio <S.suarerez@gmail.com>
2020-07-03 11:58:18 -0500

    started: added tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+
+import Join from "./components/Join";
+import Chat from "./components/Chat";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/general" component={Chat} />
+          <Route path="/" component={Join} />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
