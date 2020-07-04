d613ff7331dd20d7ed54701c274ea7b16560e7a2
Sergio <S.suarerez@gmail.com>
2020-07-04 17:22:36 -0500

    removed: console log

--- src/components/Messages/Messages.tsx
+++ src/components/Messages/Messages.tsx
@@ -11,7 +11,6 @@ export default function Messages({ messages, user }: MessagesProps) {
 
   useEffect(() => {
     executeScroll();
-    console.log("Scrolling");
   }, [messages]);
 
   const executeScroll = () => {
