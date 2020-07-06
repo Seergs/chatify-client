ce941cf6042d3da7091cc62d45ec887ed70182fa
Sergio <S.suarerez@gmail.com>
2020-07-05 19:26:53 -0500

    added: service worker

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
+
+import * as serviceWorker from "./serviceWorker";
+
 import App from "./App";
 
 import "./assets/main.css";
@@ -10,3 +13,5 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById("root")
 );
+
+serviceWorker.register();
